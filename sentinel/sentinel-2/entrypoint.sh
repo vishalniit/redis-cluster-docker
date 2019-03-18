cf4f23ec21324d9e9cefc2d898c982e84b29871c
Vishal Gupta <vishal.gupta@mindtree.com>
2019-03-18 14:02:14 -0400

    corrected docker-compose error & entrypoint for sentinel

--- sentinel/sentinel-2/entrypoint.sh
+++ sentinel/sentinel-2/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-IP=`getent hosts ${HOST} | awk '{ print $1 }'`
+#IP=`getent hosts ${HOST} | awk '{ print $1 }'`
+IP=13.71.87.72
 sed -i "s/\$SENTINEL_PORT/$SENTINEL_PORT/g" /etc/redis/sentinel.conf
 sed -i "s/\$REDIS_PASSWORD/$REDIS_PASSWORD/g" /etc/redis/sentinel.conf
 sed -i "s/\$SENTINEL_QUORUM/$SENTINEL_QUORUM/g" /etc/redis/sentinel.conf
