6e35caf627061f77eeac3ad63a17583b3e20d764
Vishal Gupta <vishal.gupta@mindtree.com>
2019-03-20 10:21:37 -0400

    cleaned code fo slaves & intro sentinel as service

--- redis-slaves/5-alpine-slave-3/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
- 
-MASTER_IP=`getent hosts ${MASTER_HOST} | awk '{ print $1 }'`
-#MYIP=`getent hosts ${HOST} | awk '{ print $1 }'`
-MYIP=13.71.87.72 
-#sed -i "s/\$MYIP/$MYIP/g" /etc/redis/redis.conf
-#sed -i "s/\$SLAVE_PORT/$SLAVE_PORT/g" /etc/redis/redis.conf
-exec docker-entrypoint.sh redis-server /etc/redis/redis.conf --slaveof ${MASTER_IP} ${MASTER_PORT} --requirepass F1tanUusDaII --appendonly yes --masterauth F1tanUusDaII
